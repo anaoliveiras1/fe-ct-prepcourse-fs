fcc8b5ff0db57c25147dc5020ff236149da01601
anaoliveiras <127421855+anaoliveiras1@users.noreply.github.com>
2023-07-11 16:08:37 -0300

    VOY POR ESPRIMO

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -132,9 +132,21 @@ function fizzBuzz(num) {
    // Si "num" es divisible entre 3 y 5 (ambos), retorna "fizzbuzz".
    // De lo contrario, retorna false.
    // Tu código:
+   // [Nota]: los números negativos, 0 y 1 NO son números primos.
+   // Tu código:
    
+   if ( num % 3 === 0 && num % 5 === 0){
+      return "fizzbuzz";
 
-}
+   } else if (num % 3 === 0){
+      return "fizz";
+
+   } else if (num % 5 === 0){
+      return "buzz";
+   } else {
+      return Boolean (false)
+   };
+};
 
 function operadoresLogicos(num1, num2, num3) {
    // La función recibe tres números distintos.
@@ -144,8 +156,19 @@ function operadoresLogicos(num1, num2, num3) {
    // Si todos los argumentos son cero, retornar ---> "Error".
    // Si no se cumple ninguna de las condiciones anteriores, retornar false.
    // Tu código:
-}
-
+   if (num1 === 0 && num2 === 0 && num3 === 0) {
+      return "Error";
+   } else if (num1 < 0 || num2 <0 || num3 <0){
+      return "Hay negativos";
+   } else if (num1 > num2 && num1 > num3 && num1 >0 ){
+      return "Numero 1 es mayor y positivo";
+   } else if ( num3 > num1 && num3 > num2 ){
+      return num3+1;
+   } else {
+      return Boolean (false)
+   };
+   };
+ 
 function esPrimo(num) {
    // Retornar true si "num" es primo.
    // De lo contrario retorna false.
